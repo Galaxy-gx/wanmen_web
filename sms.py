029eacf4af803e952a12dc5cb066214e37e14e26
gaoxt <simerpc@gmail.com>
2019-04-19 22:00:06 +0800

    add cache

--- sms.py
+++ sms.py
@@ -26,7 +26,7 @@ class passport:
         content = requests.post('https://passport.9you.com/seccode.php' + suffix, headers=headers)
 
         arr = re.findall('PHPSESSID\=([^\;].*)\;.*', content.headers['Set-Cookie'])
-        PHPSESSID = arr[0].decode('utf-8')
+        PHPSESSID = arr[0]
         print(type(arr[0]))
         print(type(PHPSESSID))
         print(type(mobile))
