cfc2703af08d4800977def3fa9fd4081c91cee98
gaoxt <simerpc@gmail.com>
2019-04-23 20:54:21 +0800

    test search_page

--- app/common.py
+++ app/common.py
@@ -6,16 +6,6 @@ import config
 from flask import jsonify
 
 
-def format_m3u8(data):
-    new_data = str(data, encoding='utf-8')
-    video_server = '//' + config.app_config.get('SERVER_URL')+'/proxy/media/'
-    it = re.finditer(".+\.ts", new_data)
-    for match in it:
-        new_data = new_data.replace(
-            match.group(), video_server + match.group())
-    return new_data
-
-
 def format_tags(arr):
     filter_arr = ['了解', '万门', '入门', '通识', '注册', '一级', '学习', '精讲', '如何', '一月', '介绍', '课程', '联考', '知识', '计划', '规划', '职业',
                   '老师', '轻松', '课堂', '简单', '素质', '时间', '教师', '小时', '分享', '体验', '九年', '预测', '超轻', '走进', '相关', '未知', '什么']
@@ -26,6 +16,16 @@ def format_tags(arr):
                 char.isdigit() for char in item.get('_id')):
             new_arr.append(item.get('_id'))
     return new_arr
+
+
+def format_m3u8(data):
+    new_data = str(data, encoding='utf-8')
+    video_server = '//' + config.app_config.get('SERVER_URL')+'/proxy/media/'
+    it = re.finditer(".+\.ts", new_data)
+    for match in it:
+        new_data = new_data.replace(
+            match.group(), video_server + match.group())
+    return new_data
 
 
 def get_page_data(data_count, page_limit, page, search):
