579984cf73a0a4063e1f4f76d0de44160188db0a
gaoxt <simerpc@gmail.com>
2019-04-27 14:04:09 +0800

    debug common

--- app/common.py
+++ app/common.py
@@ -6,6 +6,18 @@ import config
 from flask import jsonify
 
 
+def format_tags(arr):
+    filter_arr = ['了解', '万门', '入门', '通识', '注册', '一级', '学习', '精讲', '如何', '一月', '介绍', '课程', '联考', '知识', '计划', '规划', '职业',
+                  '老师', '轻松', '课堂', '简单', '素质', '时间', '教师', '小时', '分享', '体验', '九年', '预测', '超轻', '走进', '相关', '未知', '什么']
+    new_arr = []
+
+    for item in arr:
+        if item.get('_id') not in filter_arr and len(item.get('_id')) > 1 and len(item.get('_id')) < 6 and not any(
+                char.isdigit() for char in item.get('_id')):
+            new_arr.append(item.get('_id'))
+    return new_arr
+
+
 def get_page_data(data_count, page_limit, page, search):
     html = '<ul class="pagination pagination-lg">'
     page_count = int(data_count / page_limit) + 1
@@ -43,18 +55,6 @@ def format_m3u8(data):
         new_data = new_data.replace(
             match.group(), video_server + match.group())
     return new_data
-
-
-def format_tags(arr):
-    filter_arr = ['了解', '万门', '入门', '通识', '注册', '一级', '学习', '精讲', '如何', '一月', '介绍', '课程', '联考', '知识', '计划', '规划', '职业',
-                  '老师', '轻松', '课堂', '简单', '素质', '时间', '教师', '小时', '分享', '体验', '九年', '预测', '超轻', '走进', '相关', '未知', '什么']
-    new_arr = []
-
-    for item in arr:
-        if item.get('_id') not in filter_arr and len(item.get('_id')) > 1 and len(item.get('_id')) < 6 and not any(
-                char.isdigit() for char in item.get('_id')):
-            new_arr.append(item.get('_id'))
-    return new_arr
 
 
 def info_msg(code, msg=''):
