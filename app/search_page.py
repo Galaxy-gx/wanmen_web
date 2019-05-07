4f111c542f58bff3c4fa1f1b9fd93e63a80b6260
gaoxt <simerpc@gmail.com>
2019-05-07 20:56:08 +0800

    fix bug search_page

--- app/search_page.py
+++ app/search_page.py
@@ -32,6 +32,24 @@ manager = Manager()
 queue = manager.Queue()
 
 
+def get_courses_data(url):
+    response = requests.get(url, timeout=30, headers=headers).json()
+    return response['lectures']
+
+
+def process_get_item_ts(q, id, url):
+    response = requests.get(url, timeout=30, headers=headers).json()
+    ts_data = ''
+    if response.get('video', '') == '' or response['video'].get('hls', '') == '':
+        method = 0
+        m3u8_url = ''
+    else:
+        method = 1
+        m3u8_url = response['video']['hls']['pcMid']
+        ts_data = requests.get(m3u8_url, timeout=30, headers=headers).content
+    q.put([id, m3u8_url, method, ts_data])
+
+
 def m3u8_format_data(id, data, class_id, class_name, lectures_id, lectures_name):
     course = {
         '_id': id,
@@ -43,34 +61,6 @@ def m3u8_format_data(id, data, class_id, class_name, lectures_id, lectures_name)
         'children_m3u8': data[id].get('video_ts')
     }
     return course
-
-
-def get_courses_data(url):
-    response = requests.get(url, timeout=30, headers=headers).json()
-    return response['lectures']
-
-
-def get_lectures_data(class_id, class_name, response):
-    downloadAction = 1
-    courses_url = "https://api.wanmen.org/4.0/content/courses/" + \
-        response[i]['id']
-    courses_data = get_courses_data(courses_url)
-
-    for n in range(len(courses_data)):
-        num = str(n + 1)
-        print(
-            "%s %s %s/%d count:%d" % (
-                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()
-                              ), class_name, num, len(courses_data),
-                len(courses_data[n]['children'])))
-        lectures_id = courses_data[n]['id']
-        lectures_name = num + '_' + \
-            courses_data[n]['name'].replace('/', '').replace('_', '')
-
-        downloadAction, children_data = get_children_data(num, courses_data[n]['children'], class_id, class_name,
-                                                          lectures_id, lectures_name)
-
-    return downloadAction
 
 
 def format_data(data, downloadAction):
@@ -92,27 +82,6 @@ def format_data(data, downloadAction):
         'downloadAction': downloadAction,
         'downloadCount': 0
     }
-    return course
-
-
-def process_get_item_ts(q, id, url):
-    response = requests.get(url, timeout=30, headers=headers).json()
-    ts_data = ''
-    if response.get('video', '') == '' or response['video'].get('hls', '') == '':
-        method = 0
-        m3u8_url = ''
-    else:
-        method = 1
-        m3u8_url = response['video']['hls']['pcMid']
-        ts_data = requests.get(m3u8_url, timeout=30, headers=headers).content
-    q.put([id, m3u8_url, method, ts_data])
-
-
-def update_data(data):
-    del data['_id']
-    del data['createdAt']
-    del data['downloadCount']
-    course = {"$set": data}
     return course
 
 
@@ -155,6 +124,37 @@ def get_children_data(num, data, class_id, class_name, lectures_id, lectures_nam
                     m3u8_format_data(key, children, class_id, class_name, lectures_id, lectures_name))
 
     return flag, children
+
+
+def get_lectures_data(class_id, class_name, response):
+    downloadAction = 1
+    courses_url = "https://api.wanmen.org/4.0/content/courses/" + \
+        response[i]['id']
+    courses_data = get_courses_data(courses_url)
+
+    for n in range(len(courses_data)):
+        num = str(n + 1)
+        print(
+            "%s %s %s/%d count:%d" % (
+                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()
+                              ), class_name, num, len(courses_data),
+                len(courses_data[n]['children'])))
+        lectures_id = courses_data[n]['id']
+        lectures_name = num + '_' + \
+            courses_data[n]['name'].replace('/', '').replace('_', '')
+
+        downloadAction, children_data = get_children_data(num, courses_data[n]['children'], class_id, class_name,
+                                                          lectures_id, lectures_name)
+
+    return downloadAction
+
+
+def update_data(data):
+    del data['_id']
+    del data['createdAt']
+    del data['downloadCount']
+    course = {"$set": data}
+    return course
 
 
 flag = True
